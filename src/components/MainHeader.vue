785ff5345b6c8484bf4f6cdd4f6a6476a662a061
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-02 18:36:07 +0200

    create header, main nav

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,55 @@
+<template>
+
+	<header class="header">
+		<div class="header__container">
+			<div class="header__logo">
+				Логотип
+			</div>
+			<main-nav/>
+		</div>
+	</header>
+
+</template>
+
+<script>
+
+	import MainNav from "@/navigation/MainNav";
+	export default {
+		name: "MainHeader",
+		components: {
+			MainNav
+		}
+	}
+
+</script>
+
+<style lang="scss" scoped>
+
+	// IMPORTS
+
+	@import "../assets/styles/utils/vars";
+
+	.header {
+		background-color: $color-turquoise;
+		padding: 12px 30px;
+		position: sticky;
+		top: 0;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+
+		&__container {
+			display: flex;
+			width: 100%;
+			max-width: 1200px;
+			align-items: center;
+		}
+
+		&__logo {
+			color: $color-white;
+			font-size: 24px;
+		}
+
+	}
+
+</style>
---
+++ src/navigation/MainNav.vue
@@ -0,0 +1,74 @@
+<template>
+
+	<nav class="navigation">
+		<button
+			v-if="getUserInfo.length === 0"
+			class="navigation__button">
+			Войти в аккаунт
+		</button>
+
+		<div
+			v-if="getUserInfo === true"
+			class="navigation__title">
+			Продукция
+		</div>
+
+		<button
+			v-if="getUserInfo === true"
+			@click="logout()"
+			class="navigation__button">
+			Выйти из системы
+		</button>
+	</nav>
+
+</template>
+
+<script>
+
+	import { mapMutations, mapGetters } from "vuex"
+	import router from "@/router";
+
+	export default {
+		name: "MainNav",
+		computed: {
+			...mapGetters(['getUserInfo'])
+		},
+		methods: {
+			...mapMutations(['CLEAR_USER_INFO']),
+			logout() {
+				this.CLEAR_USER_INFO();
+				router.push('/login');
+			}
+		}
+	}
+
+</script>
+
+<style lang="scss" scoped>
+
+	// IMPORTS
+
+	@import "../assets/styles/utils/vars";
+
+	.navigation {
+		margin: 0 0 0 10%;
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+
+		&__title {
+			color: $color-white;
+		}
+
+		&__button {
+			background: none;
+			padding: 0;
+			margin: 0;
+			color: $color-white;
+			border: none;
+			outline: none;
+			cursor: pointer;
+		}
+	}
+
+</style>
