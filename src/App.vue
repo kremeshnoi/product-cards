62caa11f13ea354f47938c287857b9cf80f5f800
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-02 18:36:20 +0200

    fix paddings

--- src/App.vue
+++ src/App.vue
@@ -31,16 +31,16 @@
 
 <style lang="scss">
 
+	* {
+		box-sizing: border-box;
+	}
+
 	body {
 		margin: 0;
 	}
 
 	#app {
 		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
 	}
 
 </style>
