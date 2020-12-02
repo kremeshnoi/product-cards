da91826ff15bf0f81f9908d57448c07b89261c7c
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-02 15:48:32 +0200

    create main footer

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,29 @@
+<template>
+
+	<footer class="footer"></footer>
+
+</template>
+
+<script>
+
+	export default {
+		name: "MainFooter"
+	}
+
+</script>
+
+<style lang="scss" scoped>
+
+	// IMPORTS
+
+	@import "../assets/styles/utils/vars";
+
+	.footer {
+		background-color: $color-turquoise;
+		padding: 24px;
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+	}
+
+</style>
