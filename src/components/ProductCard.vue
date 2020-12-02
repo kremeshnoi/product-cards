c3d0ae834d8c9b4ea37ea581fd48ae5fe0d615eb
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-02 18:18:23 +0200

    create product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,81 @@
+<template>
+
+	<div class="card">
+		<div class="card__container">
+			<img class="card__image"
+				alt="product"
+				:src="productsPropData['Image URL']">
+
+			<div class="card__type">
+				{{ productsPropData["Type"] }}
+			</div>
+			<div class="card__sku">
+				{{ productsPropData["SKU"] }}
+			</div>
+			<div class="card__price">
+				{{ productsPropData["Retail Price"] }} грн
+			</div>
+		</div>
+	</div>
+
+</template>
+
+<script>
+
+	// COMPONENT OPTIONS
+
+	export default {
+		name: "ProductCard",
+		props: ['productsPropData']
+	}
+
+</script>
+
+<style lang="scss" scoped>
+
+	// IMPORTS
+
+	@import "../assets/styles/utils/vars";
+
+	.card {
+		border-radius: 6px;
+		border: 2px solid $color-grey-light;
+		padding: 20px;
+		cursor: pointer;
+		&:hover {
+			transition: 0.4s ease-in-out;
+			box-shadow: 1px 2px 15px -1px rgba(0,0,0,0.4);
+		}
+
+		&__container {
+			display: flex;
+			flex-direction: column;
+		}
+
+		&__image {
+			width: 100%;
+			max-width: 200px;
+			align-self: center;
+		}
+
+		&__type {
+			margin: 4px 0;
+			color: $color-grey-dark;
+			text-align: start;
+		}
+
+		&__sku {
+			margin: 4px 0;
+			font-weight: 600;
+			text-align: start;
+		}
+
+		&__price {
+			font-weight: 600;
+			font-size: 20px;
+			color: $color-green;
+			text-align: end;
+		}
+	}
+
+</style>
